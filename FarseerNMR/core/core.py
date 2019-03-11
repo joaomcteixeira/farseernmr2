a30b0195fb0ea4e6c3073f95d039931700d92d9c
joaomcteixeira <joaomcteixeira@gmail.com>
2019-03-11 18:36:30 +0100

    add core script

---
+++ FarseerNMR/core/core.py
@@ -0,0 +1,36 @@
+"""
+Contains variables and parameters that serve software-wide.
+"""
+# Copyright © 2017-2019 Farseer-NMR
+#
+# @Original Article: http://bit.ly/farseerjbnmr
+# @ResearchGate https://goo.gl/z8dPJU
+# @Twitter https://twitter.com/farseer_nmr
+# @GitHub: http://bit.ly/farseer-nmr
+# @Mailist: http://bit.ly/fs_maillist
+#
+# This file is part of the Farseer-NMR project.
+#
+# Farseer-NMR is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Farseer-NMR is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Farseer-NMR. If not, see <http://www.gnu.org/licenses/>.
+#
+# For full list of author contributors visit:
+#    https://github.com/Farseer-NMR/FarSeer-NMR/wiki/Citing
+#
+# Contributors to this file:
+#    - João M.C. Teixeira (https://github.com/joaomcteixeira)
+
+abort_message = "* ABORTING *"
+"""
+An abort message when something goes wrong.
+"""
